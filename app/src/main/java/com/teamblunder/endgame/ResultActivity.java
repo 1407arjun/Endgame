0691c03330eb698ab8bda8c474b1420219aa2960
Arjun <arjun140702@gmail.com>
2021-05-01 22:34:13 +0530

    Refactored code

--- app/src/main/java/com/teamblunder/endgame/ResultActivity.java
+++ app/src/main/java/com/teamblunder/endgame/ResultActivity.java
@@ -1,51 +1,22 @@
 package com.teamblunder.endgame;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-import com.teamblunder.endgame.api.ImageAPI;
 import com.teamblunder.endgame.fragments.KeywordFragment;
 import com.teamblunder.endgame.fragments.SummaryFragment;
-import com.teamblunder.endgame.models.ImageJSONPlaceholder;
-import com.teamblunder.endgame.models.KeywordAdapter;
 import com.teamblunder.endgame.models.TabAdapter;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ResultActivity extends AppCompatActivity {
 
     ArrayList<String> resultList = new ArrayList<>();
-    static ArrayList<String> keywordsList = new ArrayList<>();
-    static ArrayList<Map<String, String>> imagesList = new ArrayList<>();
     static String result, text;
     ProgressDialog progress;
 
@@ -58,7 +29,10 @@ public class ResultActivity extends AppCompatActivity {
         text = intent.getStringExtra("text");
         if (!text.equals("")) {
             resultList.clear();
-            keywordsList.clear();
+            KeywordFragment keywordFragment = new KeywordFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("text", text);
+            keywordFragment.setArguments(bundle);
             //Do something with the text
             if (!result.equals("")) {
                 SummaryFragment summaryFragment = new SummaryFragment();
@@ -67,12 +41,6 @@ public class ResultActivity extends AppCompatActivity {
                 summaryFragment.setArguments(args);
             }
         }
-
-        progress = new ProgressDialog(this);
-        progress.setMessage("Retrieving data");
-        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progress.setIndeterminate(true);
-        progress.setCancelable(false);
 
         TabLayout tabLayout= findViewById(R.id.tabLayout);
         ViewPager viewPager = findViewById(R.id.viewPager);
@@ -98,89 +66,5 @@ public class ResultActivity extends AppCompatActivity {
 
             }
         });
-
-
-    }
-
-    public void getData(){
-
-        AnalyzeTextTask analyzeTextTask = new AnalyzeTextTask();
-        try {
-            String urlEncoder = URLEncoder.encode(text, "UTF-8");
-            analyzeTextTask.execute("https://de6c984099e4.ngrok.io/" + urlEncoder);
-            progress.show();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-        progress.show();
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(ImageAPI.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        ImageAPI myApi = retrofit.create(ImageAPI.class);
-        for (int i = 0; i < keywordsList.size(); i++){
-            String name = keywordsList.get(i);
-            Call<ImageJSONPlaceholder> call = myApi.getResult(name, "isch", "0", "eba15b7a6b9ecd04083e7acc5a05eedcf8f727afd4d6de755e4f0b78ee6185a1");
-            call.enqueue(new Callback<ImageJSONPlaceholder>() {
-                @Override
-                public void onResponse(Call<ImageJSONPlaceholder> call, Response<ImageJSONPlaceholder> response) {
-                    ImageJSONPlaceholder searchResults = response.body();
-                    if (searchResults.getSuggestedSearches().get(0).getThumbnail() != null){
-                        Map<String, String> entry = new HashMap<>();
-                        entry.put("name", name);
-                        entry.put("thumbnail", searchResults.getSuggestedSearches().get(0).getThumbnail());
-                        imagesList.add(entry);
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<ImageJSONPlaceholder> call, Throwable t) {
-                    Toast.makeText(ResultActivity.this, "Couldn't fetch data", Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-        progress.dismiss();
-    }
-
-    class AnalyzeTextTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... urls) {
-            try {
-                URL url = new URL(urls[0]);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                InputStreamReader reader = new InputStreamReader(inputStream);
-                int data = reader.read();
-                while (data != -1) {
-                    char current = (char) data;
-                    result += current;
-                    data = reader.read();
-                }
-                return result;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            if (s != null) {
-                try {
-                    JSONArray jsonArray = new JSONArray(s);
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        keywordsList.add((String) jsonArray.get(i));
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            progress.dismiss();
-        }
     }
 }
--- app/src/main/java/com/teamblunder/endgame/fragments/KeywordFragment.java
+++ app/src/main/java/com/teamblunder/endgame/fragments/KeywordFragment.java
@@ -1,14 +1,18 @@
 package com.teamblunder.endgame.fragments;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.teamblunder.endgame.R;
 import com.teamblunder.endgame.ResultActivity;
@@ -16,7 +20,19 @@ import com.teamblunder.endgame.api.ImageAPI;
 import com.teamblunder.endgame.models.ImageJSONPlaceholder;
 import com.teamblunder.endgame.models.KeywordAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import retrofit2.Call;
@@ -27,17 +43,120 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class KeywordFragment extends Fragment {
 
+    String result;
+    ArrayList<String> keywordsList = new ArrayList<>();
+    ArrayList<Map<String, String>> imagesList = new ArrayList<>();
+    ProgressDialog progress;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_keyword, container, false);
         RecyclerView keywordRecyclerView = view.findViewById(R.id.keywordRecyclerView);
 
-        /*KeywordAdapter keywordAdapter = new KeywordAdapter(imagesList,getActivity());
+        Bundle bundle = getArguments();
+        String text = bundle.getString("text");
+
+
+        progress = new ProgressDialog(getActivity());
+        progress.setMessage("Retrieving data");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+        progress.setCancelable(false);
+
+        getData(text);
+        getImageData();
+
+        KeywordAdapter keywordAdapter = new KeywordAdapter(imagesList, getActivity());
         GridLayoutManager layoutManager = new GridLayoutManager(getActivity(),2);
         keywordRecyclerView.setLayoutManager(layoutManager);
-        keywordRecyclerView.setAdapter(keywordAdapter);*/
+        keywordRecyclerView.setAdapter(keywordAdapter);
 
         return view;
+    }
+
+    public void getData(String inputIext){
+
+        AnalyzeTextTask analyzeTextTask = new AnalyzeTextTask();
+        try {
+            String urlEncoder = URLEncoder.encode(inputIext, "UTF-8");
+            analyzeTextTask.execute("https://de6c984099e4.ngrok.io/" + urlEncoder);
+            progress.show();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getImageData(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(ImageAPI.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        progress.show();
+        ImageAPI myApi = retrofit.create(ImageAPI.class);
+        for (int i = 0; i < keywordsList.size(); i++){
+            String name = keywordsList.get(i);
+            Call<ImageJSONPlaceholder> call = myApi.getResult(name, "isch", "0", "eba15b7a6b9ecd04083e7acc5a05eedcf8f727afd4d6de755e4f0b78ee6185a1");
+            call.enqueue(new Callback<ImageJSONPlaceholder>() {
+                @Override
+                public void onResponse(Call<ImageJSONPlaceholder> call, Response<ImageJSONPlaceholder> response) {
+                    ImageJSONPlaceholder searchResults = response.body();
+                    if (searchResults.getSuggestedSearches().get(0).getThumbnail() != null){
+                        Map<String, String> entry = new HashMap<>();
+                        entry.put("name", name);
+                        entry.put("thumbnail", searchResults.getSuggestedSearches().get(0).getThumbnail());
+                        imagesList.add(entry);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ImageJSONPlaceholder> call, Throwable t) {
+                    Toast.makeText(getActivity(), "Couldn't fetch data", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+        progress.dismiss();
+    }
+
+    public class AnalyzeTextTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            result = "";
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (s != null) {
+                try {
+                    JSONArray jsonArray = new JSONArray(s);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        keywordsList.add((String) jsonArray.get(i));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            progress.dismiss();
+        }
     }
 }
